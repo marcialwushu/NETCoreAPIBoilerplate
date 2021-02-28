8a156a60097f78683603bc545735dbeb67c4a3ac
cleilson pereira <marcialwushu@hotmail.com>
2021-02-28 01:05:26 -0300

    :fire: Adiconado pacote nuget para trabalhar com Jwt

--- Application/JDPI.Application.API/Controllers/ConnectController.cs
+++ Application/JDPI.Application.API/Controllers/ConnectController.cs
@@ -1,12 +1,10 @@
 ﻿using JDPI.Application.API.Models;
-using JDPI.Common.Util.Providers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace JDPI.Application.API.Controllers
 {
@@ -19,32 +17,33 @@ namespace JDPI.Application.API.Controllers
             _config = configuration;
         }
 
-        //public JsonResult Token([FromBody] TokenRequest tokenRequest)
-        //{
-        //    return new JsonResult(GerarTokenJWT(tokenRequest));
-        //}
+        public JsonResult Token([FromBody] TokenRequest tokenRequest)
+        {
+            return new JsonResult(GerarTokenJWT(tokenRequest));
+        }
 
-        //private Token GerarTokenJWT(TokenRequest tokenRequest)
-        //{
-        //    var issuer = _config["Jwt:Issuer"];
-        //    var audience = _config["Jwt:Audience"];
-        //    var securityKey = new SymetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-        //    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        private Token GerarTokenJWT(TokenRequest tokenRequest)
+        {
+            var issuer = _config["Jwt:Issuer"];
+            var audience = _config["Jwt:Audience"];
+            //var securityKey = new (Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            //var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-        //    var token = new JwtSecurityToken(issuer, audience, expire: DateTime.Now.AddMinutes(30), signingCredentials: credentials);
+            var token = new JwtSecurityToken(issuer, audience);
 
-        //    var tokenHandler = new JwtSecurityTokenHandler();
-        //    var stringToken = tokenHandler.WriteToken(token);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var stringToken = tokenHandler.WriteToken(token);
 
-        //    Token tokenJwt = new Token()
-        //    {
-        //        access_token = stringToken,
-        //        expires_in = token.Payload.Exp.Value,
-        //        token_type = "Bearer",
-        //        scope = tokenRequest.scope
-        //    };
-        //    return tokenJwt;
+            Token tokenJwt = new Token()
+            {
+                access_token = stringToken,
+                expires_in = token.Payload.Exp.Value,
+                token_type = "Bearer",
+                scope = tokenRequest.scope
+            };
+            return tokenJwt;
 
         }
     }
 }
+
